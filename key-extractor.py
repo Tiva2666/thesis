e40f96c5be8be5d154f6915a9f198a2613b02241
Radita Liem <hesti@ut.ee>
2018-01-14 23:41:35 +0200

    Add key extractor

--- key-extractor.py
+++ key-extractor.py
@@ -14,7 +14,6 @@ used_keys = [
 	'body',
 	'object',
 	'favoritesCount',
-	'twitter_entities',
 	'twitter_filter_level',
 	'retweetCount',
 	'info',
@@ -22,9 +21,34 @@ used_keys = [
 	'inReplyTo'
 ]
 
+headers = [
+	'id',
+	'objectType',
+	'actor.id',
+	'actor.displayName',
+	'actor.followersCount',
+	'verb',
+	'postedTime',
+	'generator.link',
+	'generator.displayName',
+	'provider.link',
+	'provider.displayName',
+	'provider.objectType',
+	'link',
+	'body',
+	'object.postedTime',
+	'favoritesCount',
+	'twitter_filter_level',
+	'retweetCount',
+	'info',
+	'location',
+	'inReplyTo'
+]
+
+
 keys_object = []
 
-max_range = 1
+max_range = 3000
 
 with open('clean_tweet.txt') as f:
     content = f.readlines()
@@ -35,7 +59,7 @@ for i in range(0, max_range):
 	new_content.append(json.loads(content[i]))
 
 big_container = []
-big_container.append(used_keys)
+big_container.append(headers)
 for content in new_content:
 	container = []
 	for ky in used_keys:
@@ -43,13 +67,34 @@ for content in new_content:
 			if (isinstance(content[ky], unicode) or isinstance(content[ky], int)):
 				if (ky == 'body'):
 					container.append(content[ky].encode('utf-8'))
+				elif (ky == 'id'):
+					idstr = content[ky].split(':')
+					container.append(idstr[2])
 				else:
 					container.append(content[ky])
 			else:
-				container.append('object')
-				print (ky)
-				for key in content[ky].keys():
-					print ('-- ' + key)
+				if (ky == 'actor'):
+					idstr = content[ky]['id'].split(':')
+					container.append(idstr[2])
+					container.append(content[ky]['displayName'].encode('utf-8'))
+					container.append(content[ky]['followersCount'])
+				elif (ky == 'object'):
+					container.append(content[ky]['postedTime'])
+				elif (ky == 'generator'):
+					container.append(content[ky]['link'])
+					container.append(content[ky]['displayName'].encode('utf-8'))
+				elif (ky == 'provider'):
+					container.append(content[ky]['link'])
+					container.append(content[ky]['displayName'].encode('utf-8'))
+					container.append(content[ky]['objectType'])
+				elif (ky == 'twitter_entities'):
+					print(content[ky]['hashtags'])
+					container.append(content[ky]['user_mentions'].encode('utf-8'))
+				else: 
+					container.append('object')
+				# print (ky)
+				# for key in content[ky].keys():
+				# 	print ('-- ' + key)
 		except Exception as e:
 			container.append('-')
 	big_container.append(container)
